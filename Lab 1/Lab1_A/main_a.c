fc794cd9b6b9a72b2b1a57f89baaf12e0f88a2c7
plyboicartier <100894526+plyboicartier@users.noreply.github.com>
2023-03-25 03:26:06 -0400

    upload lab 1 part a

---
+++ Lab 1/Lab1_A/main_a.c
@@ -0,0 +1,49 @@
+/*
+*	Carter Glynn
+*	EECE2617 Embedded OS - Lab 1
+*	2023-02-12
+*/
+#include <stdio.h>
+#include <conio.h>
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "task.h"
+#include "timers.h"
+#include "queue.h"
+
+void vTask1(void);
+void vTask2(void);
+void vTask3(void);
+
+int main(void) {
+	xTaskCreate(vTask1, "TASK_1", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+	xTaskCreate(vTask2, "TASK_2", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+	xTaskCreate(vTask3, "TASK_2", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+	vTaskStartScheduler();
+	for (;;);
+	return 0;
+}
+
+void vTask1(void* pvParameters) {
+	const char* pcTaskName = "Task 1 is running\r\n";
+	while (1) {
+		printf(pcTaskName);
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	}
+}
+
+void vTask2(void* pvParameters) {
+	const char* pcTaskName = "Task 2 is running\r\n";
+	while (1) {
+		printf(pcTaskName);
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	}
+}
+
+void vTask3(void* pvParameters) {
+	const char* pcTaskName = "Task 3 is running\r\n";
+	while (1) {
+		printf(pcTaskName);
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	}
+}
